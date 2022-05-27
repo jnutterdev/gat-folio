dbb8c0d8f1af4b95c8e235ec0bf3f9a1d1ca6170
jnutterdev <hello@jnutterdev.com>
2022-05-27 16:07:33 -0400

    Started fleshing out basic portfolio using Gatsby and Tailwind

---
+++ src/config/index.js
@@ -0,0 +1,50 @@
+module.exports = {
+    author: "John Nutter",
+    siteTitle: "John Nutter - Portfolio",
+    siteShortTitle: "JN",
+    siteDescription:
+      "A personal portfolio created with Gatsby and Tailwind. John Nutter is a software engineer based out of Atlanta, GA.",
+    siteLanguage: "en_US",
+    socialMedia: [
+      {
+        name: "Twitter",
+        url: "https://twitter.com/jnutterdev",
+      },
+      {
+        name: "LinkedIn",
+        url: "https://www.linkedin.com/in/jnutterdev/",
+      },
+      {
+        name: "Medium",
+        url: "",
+      },
+      {
+        name: "GitHub",
+        url: "https://github.com/jnutterdev",
+      },
+      {
+        name: "Dev",
+        url: "",
+      },
+    ],
+    navLinks: {
+      menu: [
+        {
+          name: "About",
+          url: "/#about",
+        },
+        {
+          name: "Projects",
+          url: "/#projects",
+        },
+        {
+          name: "Contact",
+          url: "/#contact",
+        },
+      ],
+      button: {
+        name: "Get In Touch",
+        url: "/#contact",
+      },
+    },
+  }
---
+++ src/pages/index.js
@@ -0,0 +1,11 @@
+import React from "react"
+import Layout from "../components/Layout"
+
+
+export default () => {
+  return (
+    <Layout>
+      <main>This is the hero section.</main>
+    </Layout>
+  )
+}
